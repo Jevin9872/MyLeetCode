7733eb9c023dfb4a2f2cd15cd78f1e2d1f8a838a
Jevin9872 <919338982@qq.com>
2021-06-01 23:36:14 +0800

    试一试

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for item in nums:
+
+        return []
+
+nums = [1,5,6,9,7]
+target = 10
+S = Solution()
+print(S.twoSum(nums,target))
