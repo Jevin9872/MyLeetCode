e6912cbaf597799a0de05f4ebe0583ca066ec9c6
Jevin9872 <919338982@qq.com>
2021-06-03 08:25:08 +0800

    试一试

--- First Week/twoSum.py
+++ First Week/twoSum.py
@@ -5,12 +5,12 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         tmp = {}
         #enumerate函数的使用：将可遍历数据对象组合成索引序列，同时出现数据和数组下标，一般用在for循环中
-        #先把数组拆成下标位置和数值位置
-        #
-        for index, num in enumerate(nums):
-            if target - num in tmp:
-                return [tmp[target - num], index]
-            tmp[num] = index
+        #1.先把数组拆成下标位置和数值位置
+        #2.在列表中数值对应的下标位置存放它在数组中的信息
+        for index,number in enumerate(nums):
+            if target - number in tmp:
+                return [tmp[target - number],index]
+            tmp[number] = index
 
 nums = [1,5,6,9,7]
 target = 10
