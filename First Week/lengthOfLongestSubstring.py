acc16f783f4069a66082b53211bc1fbafc492968
Jevin9872 <919338982@qq.com>
2021-06-05 16:22:14 +0800

    666

--- First Week/lengthOfLongestSubstring.py
+++ First Week/lengthOfLongestSubstring.py
@@ -1,5 +1,21 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        A = ''
+        B = ''
+        Max = 0
+        for element in s:
+            A = A + element
+            B = s.lstrip()
+            for element2 in A:
+                if element2 in B:
+                    Max = len(A)
+                else:
+                    continue
+        return Max
 
-        return
+s = "abcabcbb"
+S = Solution()
+print(S.lengthOfLongestSubstring(s))
