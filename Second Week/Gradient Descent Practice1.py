ca179feb644ae5edbcfca387c52bb1690aab7733
Jevin9872 <919338982@qq.com>
2021-06-13 08:55:28 +0800

    二

---
+++ Second Week/Gradient Descent Practice1.py
@@ -0,0 +1,11 @@
+from __future__ import division
+from sympy import symbols, diff, expand
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+
+data = {'x' :[1,2,3,4,5,6,7,8,9,10], 'y' : 10} #初始化数据集
+
